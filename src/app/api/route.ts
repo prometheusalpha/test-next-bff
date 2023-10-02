c0642512fb657595f5f5defe17ff2c7598e7e44a
hoangnh280 <hoangnh280@git.ghtk.vn>
2023-10-02 14:23:18 +0700

    init

---
+++ src/app/api/route.ts
@@ -0,0 +1,16 @@
+export async function POST(request: Request) {
+  let data = await request.json();
+  let search = data.search || "No search term provided";
+
+  // send data to another API
+  let response = await fetch("https://httpbin.org/post", {
+    method: "POST",
+    body: JSON.stringify({ search: search }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let text = await response.text();
+  // send response
+  return new Response(text, { status: 200 });
+}
